082c07ef3529f06e57803331d164fe4eaf48a9ce
Deepalakshmimani479 <manikamala49@gmail.com>
2026-07-27 09:20:31 +0530

    Modified authAdmin

--- server/routes/stationRoutes.js
+++ server/routes/stationRoutes.js
@@ -5,7 +5,7 @@ import { addStation, deleteStation, updateStation,getStations } from "../control
 const stationRouter=express.Router();
 
 
-stationRouter.post("/add",authAdmin,addStation);
+stationRouter.post("/add",addStation);
 
 stationRouter.put("/update/:id",authAdmin,updateStation);
 
